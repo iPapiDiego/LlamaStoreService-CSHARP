bfe6c71ad961666d2ee9bbd090f3581d810fbb5a
iPapiDiego <diego-ggwp@hotmail.com>
2025-04-28 13:27:33 -0500

    mejoras

--- LlamaStoreVista/wwwroot/js/Index.js
+++ LlamaStoreVista/wwwroot/js/Index.js
@@ -1,1 +1,20 @@
 ﻿
+const carousel = document.getElementById('carousel');
+const slides = carousel.querySelectorAll('.slide');
+let index = 0;
+
+function scrollCarousel(direction) {
+    index += direction;
+    if (index < 0) index = slides.length - 1;
+    if (index >= slides.length) index = 0;
+
+    const slideWidth = slides[0].offsetWidth + 16; // slide + gap
+    carousel.scrollTo({
+        left: slideWidth * index,
+        behavior: 'smooth'
+    });
+}
+
+setInterval(() => {
+    scrollCarousel(1);
+}, 3000); // cambia cada 3 segundos
