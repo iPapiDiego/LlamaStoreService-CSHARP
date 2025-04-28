ed96b4ab3a5a47d52888d5af123a7acc85eec75e
iPapiDiego <diego-ggwp@hotmail.com>
2025-04-27 22:06:01 -0500

    agregando vistas

---
+++ LlamaStoreVista/Controllers/ProductosController.cs
@@ -0,0 +1,49 @@
+﻿using LlamaStoreVista.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LlamaStoreVista.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly string conexionService = "https://localhost:44331/api/Products/";
+
+        //LISTA DE VENDEDORES CON NUMERACION
+        public async Task<IActionResult> ProductosVista(int page = 1)
+        {
+            List<Producto> temporal = new List<Producto>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionService);
+                HttpResponseMessage response = await client.GetAsync("getCelulares");
+
+                if (response.IsSuccessStatusCode) // 👈 Verifica que el API respondió OK
+                {
+                    string apiresponse = await response.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrEmpty(apiresponse))
+                    {
+                        var deserialized = JsonConvert.DeserializeObject<List<Producto>>(apiresponse);
+                        if (deserialized != null)
+                            temporal = deserialized.ToList();
+                    }
+                }
+                else
+                {
+                    // Opcional: manejar el error
+                    Console.WriteLine($"Error API: {response.StatusCode}");
+                }
+            }
+
+            // Proceso para la paginación
+            int fila = 5;
+            int count = temporal.Count();
+            int pages = count % fila == 0 ? count / fila : count / fila + 1;
+            page = page - 1;
+            ViewBag.page = page;
+            ViewBag.pages = pages;
+
+            return View(await Task.Run(() => temporal.Skip(fila * page).Take(fila)));
+        }
+    }
+}
---
+++ LlamaStoreVista/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿namespace LlamaStoreVista.Models
+{
+    public class Producto
+    {
+        public string idcel { get; set; }
+        public string idmarca { get; set; }
+        public string modelo { get; set; }
+        public decimal precio { get; set; }
+        public string idgama { get; set; }
+        public int stock { get; set; }
+        public string idsistema { get; set; }
+        public string idestado { get; set; }
+    }
+}
