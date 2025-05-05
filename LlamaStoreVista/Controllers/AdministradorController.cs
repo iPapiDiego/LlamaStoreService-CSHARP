0e6fe070f10281db241e27f3f3812c977e7a4d6c
iPapiDiego <diego-ggwp@hotmail.com>
2025-05-05 15:56:49 -0500

    productos terminado

--- LlamaStoreVista/Controllers/AdministradorController.cs
+++ LlamaStoreVista/Controllers/AdministradorController.cs
@@ -1,10 +1,15 @@
 ﻿using LlamaStoreService.Models.Accesorios;
 using LlamaStoreService.Models.Auditos;
+using LlamaStoreService.Models.Estatus;
+using LlamaStoreService.Models.Productos;
+using LlamaStoreService.Models.Products;
 using LlamaStoreService.Models.Tickets;
 using LlamaStoreService.Models.Users;
 using LlamaStoreVista.Models.Product;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace LlamaStoreVista.Controllers
 {
@@ -156,7 +161,138 @@ namespace LlamaStoreVista.Controllers
             return View(await Task.Run(() => temporal.Skip(fila * page).Take(fila)));
         }
 
-        
+        //=========================================================================
 
+        public async Task<List<Gama>> ListaGamas()
+        {
+            List<Gama> temporal = new List<Gama>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                HttpResponseMessage response = await client.GetAsync("getGamas");
+                string apiresponse = await response.Content.ReadAsStringAsync();
+                temporal = JsonConvert.DeserializeObject<List<Gama>>(apiresponse).ToList();
+            }
+            return temporal;
+        }
+
+        public async Task<List<Marca>> ListaMarcas()
+        {
+            List<Marca> temporal = new List<Marca>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                HttpResponseMessage response = await client.GetAsync("GetMarcas");
+                string apiresponse = await response.Content.ReadAsStringAsync();
+                temporal = JsonConvert.DeserializeObject<List<Marca>>(apiresponse).ToList();
+            }
+            return temporal;
+        }
+
+        public async Task<List<Sistema>> ListaSistemas()
+        {
+            List<Sistema> temporal = new List<Sistema>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                HttpResponseMessage response = await client.GetAsync("getSistemas");
+                string apiresponse = await response.Content.ReadAsStringAsync();
+                temporal = JsonConvert.DeserializeObject<List<Sistema>>(apiresponse).ToList();
+            }
+            return temporal;
+        }
+
+        public async Task<List<Estado>> ListaEstados()
+        {
+            List<Estado> temporal = new List<Estado>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                HttpResponseMessage response = await client.GetAsync("getEstados");
+                string apiresponse = await response.Content.ReadAsStringAsync();
+                temporal = JsonConvert.DeserializeObject<List<Estado>>(apiresponse).ToList();
+            }
+            return temporal;
+        }
+
+        public async Task<IActionResult> AgregarProducto()
+        {
+            var gamas = await ListaGamas();
+            ViewBag.gamas = new SelectList(gamas, "idgama", "tipogama");
+
+            var marcas = await ListaMarcas();
+            ViewBag.marcas = new SelectList(marcas, "idmarca", "nombre_marca");
+
+            var sistemas = await ListaSistemas();
+            ViewBag.sistemas = new SelectList(sistemas, "idsistema", "tipodesistema");
+
+            var estados = await ListaEstados();
+            ViewBag.estados = new SelectList(estados, "idestado", "descripcion");
+
+            return View(await Task.Run(() => new ProductoCrear()));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarProducto(ProductoCrear productoCrear)
+        {
+            string mensaje = "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                StringContent content = new StringContent(JsonConvert.SerializeObject(productoCrear), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("postAgregarCelular", content);
+                string apiresponse = await response.Content.ReadAsStringAsync();
+                mensaje = apiresponse;
+            }
+            TempData["mensaje"] = mensaje;
+            return RedirectToAction("ListaProductos");
+        }
+
+        //EDITAR PRODUCTO
+        public async Task<IActionResult> ActualizarProducto(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return RedirectToAction("ListaProductos");
+
+            ProductoCrear vendedor = new ProductoCrear();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                HttpResponseMessage respons = await client.GetAsync("getBuscarCelular/?id=" + id);
+                string apiResponse = await respons.Content.ReadAsStringAsync();
+                vendedor = JsonConvert.DeserializeObject<ProductoCrear>(apiResponse);
+            }
+
+            var gamas = await ListaGamas();
+            ViewBag.gamas = new SelectList(gamas, "idgama", "tipogama");
+
+            var marcas = await ListaMarcas();
+            ViewBag.marcas = new SelectList(marcas, "idmarca", "nombre_marca");
+
+            var sistemas = await ListaSistemas();
+            ViewBag.sistemas = new SelectList(sistemas, "idsistema", "tipodesistema");
+
+            var estados = await ListaEstados();
+            ViewBag.estados = new SelectList(estados, "idestado", "descripcion");
+
+            return View(await Task.Run(() => vendedor));
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ActualizarProducto(ProductoCrear productoCrear)
+        {
+            string mensaje = "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(conexionProduc);
+                StringContent content = new StringContent(JsonConvert.SerializeObject(productoCrear), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("postActualizarCelular", content);
+                string apiresponse = await response.Content.ReadAsStringAsync();
+                mensaje = apiresponse;
+            }
+            TempData["mensaje"] = mensaje;
+            return RedirectToAction("ListaProductos");
+        }
     }
 }
